3049e2b8c27f3dc02b1fc148d37fb7e62970540b
Edylson Torikai <tedylson@gmail.com>
2018-09-20 21:18:46 -0300

    implementa programa para n processos

--- programa.c
+++ programa.c
@@ -5,39 +5,68 @@
 // 9293007 Fernando Vinícius Gianini Silva
 // 7151905 Romeu Bertho Junior
 
+/*
+Particionamento
+Cada tarefa procura a maior diferença entre um elemento e seus vizinhos 
+
+Comunicação
+A comunicação acontece ao se agrupar todas as diferenças encontradas no processo principal
+
+Aglomeração
+A aglomeração depende da quantidade de processos estabelecidos e da ordem N da matriz.
+O programa tenta disponibilizar um número próximo de tarefas para cada processo.
+
+Mapeamento
+O mapeamento é de acordo com o numero de processadores disponibilizados.
+
+
+
+*/
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <limits.h>
 #include <math.h>
 #include <mpi.h>
 
-#define n_processos 2
 
-void calc_diferenca(int **matriz, int linha_maior,int coluna_maior,int *linha_menor,int
-	*coluna_menor,int *valor_maior,int *valor_menor){
-	int dif, aux_dif = INT_MIN;
+
+void calc_diferenca(int **matriz, int linha_maior,int coluna_maior){
+	int linha_menor,coluna_menor,valor_maior,valor_menor;
+	int dif = INT_MIN, aux_dif = INT_MIN;
 	int i, j;
+	int mensagem[5];
+
 	for (i = linha_maior - 1; i < linha_maior + 2; i++) {
 		for (j = coluna_maior - 1; j < coluna_maior + 2; j++) {
 		
 			if (!(i < 0 || i > linha_maior || j < 0 || j > coluna_maior)){
 				aux_dif = abs(matriz[linha_maior][coluna_maior] - matriz[i][j]);
-			if (aux_dif > dif){
-				dif = aux_dif;
-				*linha_menor=i;
-				*coluna_menor=j;
-				*valor_maior=matriz[linha_maior][coluna_maior];
-				*valor_menor=matriz[i][j];
+				if (aux_dif > dif){
+					dif = aux_dif;
+					linha_menor=i;
+					coluna_menor=j;
+					valor_maior=matriz[linha_maior][coluna_maior];
+					valor_menor=matriz[i][j];
+				}
 			}
-		}
+		}	
 	}
 
-		MPI_Send(&dif, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-		MPI_Send(linha_menor, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-		MPI_Send(coluna_menor, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
-		MPI_Send(valor_maior, 1, MPI_INT, 0, 4, MPI_COMM_WORLD);
-		MPI_Send(valor_menor, 1, MPI_INT, 0, 5, MPI_COMM_WORLD);
-	}
+	
+	mensagem[0] = dif;
+	mensagem[1] = linha_menor;
+	mensagem[2] = coluna_menor;
+	mensagem[3] = valor_maior;
+	mensagem[4] = valor_menor;
+
+
+
+
+	MPI_Send(mensagem, 5, MPI_INT, 0, 1, MPI_COMM_WORLD);
 }
 
 int main(int argc, char *argv[]){
@@ -57,7 +86,7 @@ for(i=0;i<tam;i++){
 		fscanf(arquivo_entrada, "%d\n", &(matriz[i][j]));
 	}
 }
-int a[10], b[10];
+int mensagem[5];
 int npes;
 int myrank;
 MPI_Status status;
@@ -73,20 +102,25 @@ int diferenca = INT_MIN;
 int pos_i, pos_j;
 for (i = 0; i < tam; i++) {
 	for (j = 0; j < tam; j++) {
-		if(myrank != 0){
-			calc_diferenca(matriz, i, j,&linha_menor_aux,&coluna_menor_aux,&valor_maior_aux,&valor_menor_aux);
+		if(myrank == ((i*tam + j)%(npes-1)+1) ){
+
+
+			calc_diferenca(matriz, i, j);
 		}
 		else if(myrank ==0) {
-			MPI_Recv(&aux_comp, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-			MPI_Recv(&linha_menor, 1, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
-			MPI_Recv(&coluna_menor, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
-			MPI_Recv(&valor_maior, 1, MPI_INT, MPI_ANY_SOURCE, 4, MPI_COMM_WORLD, &status);
-			MPI_Recv(&valor_menor, 1, MPI_INT, MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status);
-
-			if (diferenca < aux_comp){
-				diferenca = aux_comp;
+			MPI_Recv(mensagem, 5, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+			
+			
+			if (diferenca < mensagem[0]){
+				diferenca = mensagem[0];
 				linha_maior=i;
 				coluna_maior=j;
+				linha_menor=mensagem[1];
+				coluna_menor=mensagem[2];
+				valor_maior=mensagem[3];
+				valor_menor=mensagem[4];
+
+
 			}
 
 
@@ -101,11 +135,14 @@ for(i=0;i<tam;i++){
 	free(matriz[j]);
 }
 
+
+
 if (myrank == 0)
-	printf("M[%d,%d]=%d M[%d,%d]=%d\n",linha_maior,coluna_maior,valor_maior,linha_menor,coluna_menor,valor_menor);
+	printf("M[%d,%d]=%d M[%d,%d]=%d",linha_maior,coluna_maior,valor_maior,linha_menor,coluna_menor,valor_menor);
 
 free(matriz);
 fclose(arquivo_entrada);
+
 
 MPI_Finalize();
 }
